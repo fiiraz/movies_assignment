80ee6cc7e0c8899779791c3833d4941b97a6efd2
özgür <ozgurpir14@gmail.com>
2024-06-05 20:06:18 +0300

    uniq it

--- app/jobs/import_movie_csv_job.rb
+++ app/jobs/import_movie_csv_job.rb
@@ -4,22 +4,37 @@ class ImportMovieCsvJob < ApplicationJob
   queue_as :default
 
   def perform(csv_file)
-    batch_size = 1000
+    movies_data = {}
+    CSV.foreach(csv_file, headers: true) do |row|
+      title = row["Movie"]
+      if movies_data[title].nil?
+        movies_data[title] = {
+          description: row["Description"],
+          year: row["Year"],
+          director: [row["Director"]],
+          actors: [row["Actor"]],
+          filming_location: [row["Filming location"]],
+          country: row["Country"]
+        }
+      else
+        movies_data[title][:director] << row["Director"]
+        movies_data[title][:actors] << row["Actor"]
+        movies_data[title][:filming_location] << row["Filming location"]
+      end
+    end
 
-    CSV.foreach(csv_file, headers: true).each_slice(batch_size) do |rows|
-      Movie.transaction do
-        rows.each do |row|
-          row = row.to_hash
-          Movie.create!(
-            title: row["Movie"],
-            description: row["Description"],
-            year: row["Year"],
-            director: row["Director"],
-            actor: row["Actor"],
-            filming_location: row["Filming location"],
-            country: row["Country"]
-          )
-        end
+    # Create movie entries in the database
+    Movie.transaction do
+      movies_data.each do |title, data|
+        Movie.create!(
+          title: title,
+          description: data[:description],
+          year: data[:year],
+          director: data[:director].uniq.join(", "),
+          actor: data[:actors].uniq.join(", "),
+          filming_location: data[:filming_location].uniq.join(", "),
+          country: data[:country]
+        )
       end
     end
   end
