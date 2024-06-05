cdd41bb0fec5499b4fe0f778f575683b6c48a3ab
özgür <ozgurpir14@gmail.com>
2024-06-05 19:40:15 +0300

    added batches

--- app/jobs/import_movie_csv_job.rb
+++ app/jobs/import_movie_csv_job.rb
@@ -4,17 +4,23 @@ class ImportMovieCsvJob < ApplicationJob
   queue_as :default
 
   def perform(csv_file)
-    CSV.foreach(csv_file, headers: true) do |row|
-      row = row.to_hash
-      Movie.create!(
-        title: row["Movie"],
-        description: row["Description"],
-        year: row["Year"],
-        director: row["Director"],
-        actor: row["Actor"],
-        filming_location: row["Filming location"],
-        country: row["Country"]
-      )
+    batch_size = 1000
+
+    CSV.foreach(csv_file, headers: true).each_slice(batch_size) do |rows|
+      Movie.transaction do
+        rows.each do |row|
+          row = row.to_hash
+          Movie.create!(
+            title: row["Movie"],
+            description: row["Description"],
+            year: row["Year"],
+            director: row["Director"],
+            actor: row["Actor"],
+            filming_location: row["Filming location"],
+            country: row["Country"]
+          )
+        end
+      end
     end
   end
 end
